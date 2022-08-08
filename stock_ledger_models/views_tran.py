f501a38d1dbea5828d9ace49f9f2cc394fdcb7de
Tarunvardhan <tarun.v@proxima360.com>
2022-08-08 22:38:33 +0530

    some changes for null values

--- stock_ledger_models/views_tran.py
+++ stock_ledger_models/views_tran.py
@@ -95,6 +95,7 @@ def trn_data_table(request):
                 results55=pd.read_sql(query,connection)
             res_list=[]
             rec={}
+            results55 =  results55.replace(np.NaN, None, regex=True)
             for val2 in results55.values:
                 count=0
                 for col4 in results55.columns:
